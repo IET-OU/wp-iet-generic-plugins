66e439e1edcc7218194370aad3dffad628d78928
Nick Freear <nfreear@yahoo.co.uk>
2015-05-19 18:24:39 +0100

    Bug #9, Customizations for CleanPrint WP plugin [LACE] [iet:3377484]
    * Javascript injection etc. Plus CSS.

--- js/lace-javascript.js
+++ js/lace-javascript.js
@@ -4,7 +4,11 @@
 
 jQuery(function ($) {
 
-  var vis_table_row_sel = ".hypothesis #country-vis-table tbody tr";
+  var $inject_cleanprint = $("body.search-results, body.single-hypothesis")
+    , clearprint_post_re = /(search-results|postid-\d+)/
+    , cleanprint_exclude_sel = "#cookie-notice, .nav-menu, .assistive-text, .jxl-message, .oer-chart-loading, .leaflet-control-container, #X-secondary"
+    , cleanprint_include_sel = ".page-title, .entry-title, #X-primary"
+    , vis_table_row_sel = ".hypothesis #country-vis-table tbody tr";
 
 
   /* Fix for blank country-cell in [hypothesis_geosummary] table [Bug: #6]
@@ -32,6 +36,33 @@ jQuery(function ($) {
     });
 
   });
+
+
+  /* CleanPrint customizations [Bug: #9]
+  */
+  if ($inject_cleanprint.length) {
+    var post_id = $("body").attr("class").match(clearprint_post_re);
+    post_id = post_id && post_id[1];
+
+    $(".page-header, .entry-header:first").first().after(
+    '<div style="text-align:right;"><a href="." onClick=\"WpCpCleanPrintPrintHtml(\'%s\');return false" title="Print page" class="cleanprint-exclude"><img src="http://lace/wp-content/plugins/cleanprint-lt/images/CleanPrintBtn_white.png" alt="Print page"/></a></div>'.replace("%s", post_id || 'custom'));
+
+    console.log("Inject CleanPrint:", $inject_cleanprint);
+  }
+
+  $(cleanprint_exclude_sel).addClass("cleanprint-exclude");
+  $(cleanprint_include_sel).addClass("cleanprint-include");
+
+  $(".oer-chart-loading").first().after(
+    "<p class='x-cleanprint-diagram-warn cleanprint-include'>[ Diagrams/ maps may not print or export well. Sorry! ]</p>");
+
+  $cleanprint_bn_wrap = $("a[ onclick ^= WpCpCleanPrint ]").parent();
+  $cleanprint_bn_wrap.addClass("x-cleanprint-buttons");
+  $cleanprint_bn_wrap.find("a[onclick]").attr("role", "button");
+
+  $("a[ onclick ^= WpCpCleanPrintPrint ]").attr("title",
+    "Print/export page to rich-text, PDF & other options");
+
 
 
   function when_call(when_true_FN, callback_FN, interval) {
